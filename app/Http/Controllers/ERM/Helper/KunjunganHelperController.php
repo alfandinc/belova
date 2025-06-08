0f21cda6dddc8b4486ac42373b15bbfacf841720
alfandinc <alfandinc@gmail.com>
2025-06-08 15:07:06 +0700

    fix bug helper kunjungan

--- app/Http/Controllers/ERM/Helper/KunjunganHelperController.php
+++ app/Http/Controllers/ERM/Helper/KunjunganHelperController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ERM\MetodeBayar;
 use App\Models\ERM\Dokter;
+use App\Models\ERM\Klinik;
 use App\Models\ERM\Visitation;
 
 class KunjunganHelperController extends Controller
@@ -16,6 +17,7 @@ class KunjunganHelperController extends Controller
         $pasienName = $visitation->pasien->nama;
         $metodeBayar = MetodeBayar::all(); // ambil semua data metode bayar
         $dokters = Dokter::with('spesialisasi')->get(); // ambil semua dokter
+        $kliniks = Klinik::all();
 
         // dd($pasienName, $metodeBayar, $dokters);
 
@@ -24,6 +26,7 @@ class KunjunganHelperController extends Controller
             'dokters' => $dokters,
             'metodeBayar' => $metodeBayar,
             'pasienName' => $pasienName,
+            'kliniks' => $kliniks,
         ];
     }
 }
