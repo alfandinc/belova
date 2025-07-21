ba83c70f4a101332096fb246a8d344c98a776012
alfandinc <alfandinc@gmail.com>
2025-07-21 15:42:25 +0700

    update view dokter to hrd

--- resources/views/erm/dokters/index.blade.php
+++ resources/views/erm/dokters/index.blade.php
@@ -1,24 +1,32 @@
-@extends('layouts.erm.app')
-@section('title', 'Data Dokter')
+
+@extends('layouts.hrd.app')
+@section('title', 'HRD | Daftar Dokter')
+@section('navbar')
+    @include('layouts.hrd.navbar')
+@endsection
 
 @section('content')
 <div class="container-fluid">
-    <div class="card">
-        <div class="card-header bg-primary text-white">
-            <h4 class="card-title">Data Dokter</h4>
+    <div class="card shadow">
+        <div class="card-header bg-white d-flex justify-content-between align-items-center py-3">
+            <h3 class="card-title m-0 font-weight-bold text-primary">Daftar Dokter</h3>
+            <a href="{{ route('erm.dokters.create') }}" class="btn btn-primary">
+                <i class="fas fa-plus mr-1"></i> Tambah Dokter
+            </a>
         </div>
         <div class="card-body">
-            <a href="{{ route('erm.dokters.create') }}" class="btn btn-success mb-3">+ Tambah Dokter</a>
-            <table class="table table-bordered" id="dokter-table">
-                <thead>
-                    <tr>
-                        <th>Nama Dokter</th>
-                        <th>Spesialisasi</th>
-                        <th>SIP</th>
-                        <th>Aksi</th>
-                    </tr>
-                </thead>
-            </table>
+            <div class="table-responsive">
+                <table id="dokter-table" class="table table-bordered table-hover table-striped">
+                    <thead class="thead-light">
+                        <tr>
+                            <th>Nama Dokter</th>
+                            <th>Spesialisasi</th>
+                            <th>SIP</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
         </div>
     </div>
 </div>
@@ -30,6 +38,21 @@ $(function () {
     $('#dokter-table').DataTable({
         processing: true,
         serverSide: true,
+        responsive: true,
+        dom: '<"top"fl>rt<"bottom"ip><"clear">',
+        language: {
+            processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span>',
+            search: "Cari:",
+            lengthMenu: "Tampilkan _MENU_ entri",
+            info: "Menampilkan _START_ sampai _END_ dari _TOTAL_ entri",
+            infoEmpty: "Menampilkan 0 sampai 0 dari 0 entri",
+            infoFiltered: "(disaring dari _MAX_ entri keseluruhan)",
+            paginate: {
+                previous: '<i class="fas fa-chevron-left"></i>',
+                next: '<i class="fas fa-chevron-right"></i>'
+            },
+            emptyTable: 'Tidak ada data yang tersedia'
+        },
         ajax: "{{ route('erm.dokters.index') }}",
         columns: [
             { data: 'nama_dokter', name: 'user.name' },
