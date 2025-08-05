cf26f1bc5a986d3b0af29a2d363735435a126428
alfandinc <alfandinc@gmail.com>
2025-08-05 16:29:50 +0700

    update navbar erm

--- resources/views/layouts/erm/navbar.blade.php
+++ resources/views/layouts/erm/navbar.blade.php
@@ -111,7 +111,7 @@
                                 <a href="/erm/fakturpembelian/create"><i data-feather="plus-square" class="align-self-center menu-icon"></i><span> Add Faktur Pembelian</span></a>
                             </li>
                             <li>
-                                <a href="/erm/obat-keluar"><i data-feather="log-out" class="align-self-center menu-icon"></i><span>Obat Keluar</span></a>
+                                <a href="/erm/obat-keluar"><i data-feather="log-out" class="align-self-center menu-icon"></i><span>Log Obat Keluar</span></a>
                             </li>
                             <li>
                                 <a href="/erm/obat"><i data-feather="package" class="align-self-center menu-icon"></i><span>Stok Obat</span></a>
